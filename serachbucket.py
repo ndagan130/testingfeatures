cbafd9f3ca5e71509319690e53d4c00a4834990c
ndagan130 <129836016+ndagan130@users.noreply.github.com>
2023-04-04 16:18:21 +0300

    Update serachbucket.py

--- serachbucket.py
+++ serachbucket.py
@@ -2,8 +2,8 @@ import boto3
 
 client = boto3.client(
     's3',
-    aws_access_key_id=AKIAYKS7MSSWWEFNRHFG,
-    aws_secret_access_key=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
+    aws_access_key_id=AKIAYKS7MSSWZEFNRHFG,
+    aws_secret_access_key=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
 )
 
 print(client.list_buckets())
