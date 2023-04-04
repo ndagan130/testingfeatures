9f8fcd92233881cb9e8f8f4ab15adfb907587129
ndagan130 <129836016+ndagan130@users.noreply.github.com>
2023-04-04 12:43:52 +0300

    Update serachbucket.py

--- serachbucket.py
+++ serachbucket.py
@@ -1,3 +1,9 @@
 import boto3
 
+client = boto3.client(
+    's3',
+    aws_access_key_id=AKIAYKS7MSSWWEFNRHFG,
+    aws_secret_access_key=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
+)
+
 print(client.list_buckets())
